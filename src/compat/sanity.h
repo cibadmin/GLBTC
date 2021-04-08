03f2e4f5804c3152f59237ef509c7502397cac49
cibadmin <61462749+cibadmin@users.noreply.github.com>
2021-04-08 18:39:17 +0300

    Add files via upload

---
+++ src/compat/sanity.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2009-2014 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef GLOBALBITCOIN_COMPAT_SANITY_H
+#define GLOBALBITCOIN_COMPAT_SANITY_H
+
+bool glibc_sanity_test();
+bool glibcxx_sanity_test();
+
+#endif // GLOBALBITCOIN_COMPAT_SANITY_H
